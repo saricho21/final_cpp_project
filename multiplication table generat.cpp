ada4e7265eac90200d802d8e4a9e6c3698a977f0
Sara Tamalew <alebelsara2@gmail.com>
2026-05-09 21:09:45 -0700

    Refactor multiplication table generator code

--- multiplication table generat.cpp
+++ multiplication table generat.cpp
@@ -21,3 +21,11 @@ int main() {
     } while (run == 'y' || run == 'Y');
     return 0;
 }
+/*Multiplication taple out put
+Enter N: 5
+Table 1: 1 2 3 4 5 6 7 8 9 10 
+Table 2: 2 4 6 8 10 12 14 16 18 20 
+Table 3: 3 6 9 12 15 18 21 24 27 30 
+Table 4: 4 8 12 16 20 24 28 32 36 40 
+Table 5: 5 10 15 20 25 30 35 40 45 50 
+Repeat? (y/n): */
