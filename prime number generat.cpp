49953bb0a52dfaa7334280d63a673ece30780436
Sara Tamalew <alebelsara2@gmail.com>
2026-05-10 21:01:48 -0700

    Refactor comments in prime number generator
    Updated comments and output format in prime number generator.

--- prime number generat.cpp
+++ prime number generat.cpp
@@ -16,6 +16,7 @@ int main() {
     }
     return 0;
 }
-/*prime number generat out put
+/*Sample prime number generat out put
 N: 50
-2 3 5 7 11 13 17 19 23 29 31 37 41 43 47 */
+2 3 5 7 11 13 17 19 23 29 31 37 41 43 47
+...............*/
